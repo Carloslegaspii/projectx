057919a673d0206d0649f749017caf79e176b36d
Carloslegaspii <105844304+Carloslegaspii@users.noreply.github.com>
2022-08-16 16:06:29 +0800

    project x

---
+++ projectx/carlos/src/App.java
@@ -0,0 +1,44 @@
+//import java.util.*;
+//public class App {
+//public static void main(String[] args) throws Exception {
+      
+      
+        //String[] mytop5touristdestination = {"newyork","japan", "paris" , "indonesia", "colombia" };
+    
+    
+    
+        // most favotite
+       //System.out.println(mytop5touristdestination[0]);
+
+       // least favorite
+      //System.out.println(mytop5touristdestination[4]);
+
+     // for (int i = 0; 1 < 5; i++) {
+       //System.out.println(mytop5touristdestination [i]);
+    
+    //Stack <String> stk = new Stack<>();
+
+    //stk.push("red book");
+    //stk.push("orange book");
+    //stk.push("white book");
+
+    //stk.pop();
+    //System.out.println(stk);
+
+  //  Queue <String> hotelreservation = new LinkedList <> ();
+   // hotelreservation.add("joshua");
+    //hotelreservation.add("alejo");
+    //hotelreservation.add("lorraine");
+   // hotelreservation.add("jacob");
+   // hotelreservation.add("sean");
+
+  //  System.out.println("The queue is: " + hotelreservation);
+   // hotelreservation.remove();
+   // System.out.println("The queue is: " + hotelreservation);
+     
+      }
+
+}
+
+
+
